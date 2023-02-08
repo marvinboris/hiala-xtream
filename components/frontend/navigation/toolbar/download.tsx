1c9416982d8099e26fc1d53311372c57ecca5bcd
Brainer21 <jarisl.ultio.21@gmail.com>
2023-02-08 10:44:41 +0100

    Backdrop download links modal

--- components/frontend/navigation/toolbar/download.tsx
+++ components/frontend/navigation/toolbar/download.tsx
@@ -17,12 +17,12 @@ export default function Download() {
     return <Popover>
         {({ close }) => (
             <>
-                <Popover.Button>
+                <Popover.Button className="outline-none">
                     <ArrowDownTrayIcon className='w-8' />
                 </Popover.Button>
                 <Transition as={Fragment} enter="transition ease-out duration-200" enterFrom="opacity-0 translate-y-1" enterTo="opacity-100 translate-y-0" leave="transition ease-in duration-150" leaveFrom="opacity-100 translate-y-0" leaveTo="opacity-0 translate-y-1">
-                    <Popover.Panel className="fixed w-screen h-screen bg-black/20 backdrop-blur-sm backdrop-filter flex items-center justify-center top-0 left-0 z-40">
-                        <Popover.Overlay onClick={close} className="fixed z-0 w-screen h-screen" />
+                    <Popover.Panel className="fixed w-screen h-screen flex items-center justify-center top-0 left-0 z-40">
+                        <Popover.Overlay onClick={close} className="fixed inset-0 z-0 w-screen h-screen bg-black/20" />
                         <div className="grid grid-cols-2 gap-2 relative z-10">
                             {[
                                 { name: 'Hiala C', os: 'Android', href: '/files/hiala-c.apk' },
