9646a48e13172cdc1179c8852ec551391f02cc2a
Brainer21 <jarisl.ultio.21@gmail.com>
2023-02-17 14:40:01 +0100

    Debugging

--- pages/api/player/series/streams/[id].ts
+++ pages/api/player/series/streams/[id].ts
@@ -19,6 +19,7 @@ export default async function handler(
     // ffmpeg.setFfmpegPath(ffmpegPath)
 
     try {
+        console.log('req.cookies.user', req.cookies.user)
         const decrypted = decryptPayload(req.cookies.user!)
         if (!decrypted) return res.status(401).json({ error: "Not authorized!" })
 
