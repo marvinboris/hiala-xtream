4f5a5575dbab4619b8089573f3471da4591e7c60
Brainer21 <jarisl.ultio.21@gmail.com>
2023-02-17 16:26:59 +0100

    got.stream LOL

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,8 @@
-import { createReadStream, createWriteStream, existsSync, mkdirSync, rmSync, statSync } from "fs"
+import { createReadStream } from "fs"
 import path from "path"
-import { promisify } from "util"
 
 import got from "got"
 import { NextApiRequest, NextApiResponse } from "next"
-import stream from 'stream'
 
 const send = (res: NextApiResponse) => {
     const readStream = createReadStream(path.join(process.cwd(), 'public', 'images', 'favicon.svg'))
@@ -21,32 +19,5 @@ type AssetsParams = {
 
 export const assets = async ({ src, req, res }: AssetsParams) => {
     if (!src) return send(res)
-
-    const pipeline = promisify(stream.pipeline)
-
-    const parsedSrc = src.split('/')
-
-    const directory = path.join(process.cwd(), 'public', 'files', parsedSrc[3])
-    const fileName = parsedSrc[parsedSrc.length - 1]
-
-    const filePath = path.join(directory, fileName)
-
-    if (!existsSync(directory)) mkdirSync(directory)
-
-    if (!existsSync(filePath)) {
-        const downloadStream = got.stream(src, {
-            headers: req.headers,
-        })
-        const fileWriterStream = createWriteStream(filePath)
-        await pipeline(downloadStream, fileWriterStream)
-
-        if (parsedSrc[3] === 'hls') setTimeout(() => {
-            rmSync(filePath)
-        }, 60 * 1000);
-    }
-    const readStream = createReadStream(filePath)
-    const stat = statSync(filePath)
-
-    if (stat.size > 0) return readStream.pipe(res)
-    else send(res)
+    return got.stream(src, { headers: req.headers }).pipe(res)
 }
--- pages/api/player/vod/streams/[id].ts
+++ pages/api/player/vod/streams/[id].ts
@@ -1,10 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import { createReadStream, existsSync, mkdirSync, readFileSync } from 'fs'
-// import path from 'path'
-
-// import axios from 'axios'
-// import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg'
-// import ffmpeg from 'fluent-ffmpeg'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { decryptPayload, handleError } from '../../../../../app/helpers/utils'
@@ -16,8 +10,6 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<unknown | { error: string }>
 ) {
-    // ffmpeg.setFfmpegPath(ffmpegPath)
-
     try {
         if (!req.cookies.user) return res.status(401).json({ error: 'Unauthorized' })
 
@@ -33,50 +25,6 @@ export default async function handler(
 
         const src = `${process.env.XTREAM_HOSTNAME!}/movie/${username}/${password}/${id}.${movie!.target_container[0]}`
         return await assets({ src, req, res })
-
-        // const parsedSrc = src.split('/')
-
-        // const directory = path.join(process.cwd(), 'public', 'files', parsedSrc[3])
-        // const fileName = `${parsedSrc.filter((_, i) => i > 3).join('-')}.mp4`
-        // const fileName = `${parsedSrc.filter((_, i) => i > 3).join('-')}.m3u8`
-
-        // const filePath = path.join(directory, fileName)
-        // if (!existsSync(directory)) mkdirSync(directory)
-        // if (!existsSync(filePath)) {
-        //     ffmpeg(src, { timeout: 432000 }).addOptions([
-        //         '-profile:v baseline', // baseline profile (level 3.0) for H264 video codec
-        //         '-level 3.0',
-        //         // '-s 640x360',          // 640px width, 360px height output video dimensions
-        //         '-start_number 0',     // start the first .ts segment at index 0
-        //         '-hls_time 5',        // 5 second segment duration
-        //         '-hls_list_size 0',    // Maxmimum number of playlist entries (0 means all entries/infinite)
-        //         '-f hls'               // HLS format
-        //     ]).output(filePath).run()
-
-        //     return res.end()
-        // }
-
-        // const source = readFileSync(filePath, 'utf8')
-
-        // const expression = new RegExp(`${username}-${password}-${id}`, "g")
-        // const result = source.replace(expression, `/files/movie/${username}-${password}-${id}`)
-
-        // res.send(result)
-
-
-
-        // const send = () => {
-        //     const readStream = createReadStream(filePath)
-        //     readStream.pipe(res)
-        // }
-
-        // if (!existsSync(filePath)) ffmpeg(src).addOptions([
-        //     '-codec copy',        // 5 second segment duration
-        // ]).output(filePath).on('end', () => {
-        //     console.log('Conversion done !')
-        //     send()
-        // }).run()
-        // else send()
     } catch (error) {
         handleError(res, error)
     }
