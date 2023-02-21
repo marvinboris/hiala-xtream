a1888419d8d0116cd610387357f8ae40da4f5a9f
Brainer21 <jarisl.ultio.21@gmail.com>
2023-02-21 19:27:32 +0100

    Debugging

--- components/error-boundary.tsx
+++ components/error-boundary.tsx
@@ -1,7 +1,7 @@
 import { Component, ExoticComponent, ReactNode } from 'react'
 
 class ErrorBoundary extends Component<{ FallbackComponent: ExoticComponent, children: ReactNode }> {
-    state = { hasError: false }
+    state = { hasError: false, error: null, errorInfo: null }
     static getDerivedStateFromError() {
         // Update state so the next render will show the fallback UI
 
@@ -10,6 +10,7 @@ class ErrorBoundary extends Component<{ FallbackComponent: ExoticComponent, chil
     componentDidCatch(error: any, errorInfo: any) {
         // You can use your own error logging service here
         console.log({ error, errorInfo })
+        this.setState({ error, errorInfo })
     }
     render() {
         // Check if the error is thrown
@@ -21,6 +22,8 @@ class ErrorBoundary extends Component<{ FallbackComponent: ExoticComponent, chil
                     <button type="button" onClick={() => this.setState({ hasError: false })}>
                         Try again?
                     </button>
+                    {this.state.error ? <p>Error: {this.state.error}</p> : null}
+                    {this.state.errorInfo ? <p>Error info: {this.state.errorInfo}</p> : null}
                 </div>
             )
         }
