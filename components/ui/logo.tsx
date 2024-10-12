5f91edfbaa8433a2824fb471ddb4b4a748c977bc
Brainer21 <jarisl.ultio.21@gmail.com>
2024-10-12 14:03:43 +0100

    new logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,14 +1,34 @@
-import { TvIcon } from "@heroicons/react/24/outline"
+import { TvIcon } from "@heroicons/react/24/outline";
 
-import { classNames } from "../../app/helpers/utils"
-import { useWindowSize } from "../../app/hooks"
+import { classNames } from "../../app/helpers/utils";
+import { useWindowSize } from "../../app/hooks";
 
 export default function Logo({ reset = false }) {
-    const { width } = useWindowSize()
-    const mobile = width !== undefined && width < 1024
+  const { width } = useWindowSize();
+  const mobile = width !== undefined && width < 1024;
 
-    return <span className={classNames("font-bold leading-none flex items-center space-x-2.5 text-transparent bg-clip-text bg-gradient-to-r", reset ? "from-primary-800 to-teal" : " from-white to-[#DFE9EC]", mobile ? "text-[26px]" : "text-[35px]")}>
-        <span>HIALA TV</span>
-        <TvIcon className={classNames(reset ? "text-teal" : "text-white", mobile ? "w-6" : "w-8")} />
+  return (
+    <img
+      src={reset ? "/images/logo.png" : "/images/logo-white.png"}
+      className={mobile ? "h-[26px]" : "h-[35px]"}
+    />
+  );
+
+  return (
+    <span
+      className={classNames(
+        "font-bold leading-none flex items-center space-x-2.5 text-transparent bg-clip-text bg-gradient-to-r",
+        reset ? "from-primary-800 to-teal" : " from-white to-[#DFE9EC]",
+        mobile ? "text-[26px]" : "text-[35px]"
+      )}
+    >
+      <span>HIALA TV</span>
+      <TvIcon
+        className={classNames(
+          reset ? "text-teal" : "text-white",
+          mobile ? "w-6" : "w-8"
+        )}
+      />
     </span>
+  );
 }
